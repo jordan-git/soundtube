3d0b6d7ee770a854045167a317741d994e419413
Jordan <jordanquinlan734@gmail.com>
2020-01-29 11:07:54 +0000

    Database changes

--- src/modules/setup_database.js
+++ src/modules/setup_database.js
@@ -10,8 +10,8 @@ const db = mysql.createConnection({
 db.connect(err => {
     if (err) throw err;
 
-    let sql = 'CREATE DATABASE IF NOT EXISTS soundtube';
-    db.query(sql, (err, result) => {
+    let sql1 = 'CREATE DATABASE IF NOT EXISTS soundtube';
+    db.query(sql1, (err, result) => {
         if (err) throw err;
     });
 
@@ -20,13 +20,20 @@ db.connect(err => {
         if (err) throw err;
     });
 
-    let sql3 =
-        'CREATE TABLE IF NOT EXISTS users (id int PRIMARY KEY AUTO_INCREMENT, username VARCHAR(24), password VARCHAR(24))';
+    let sql3 = `CREATE TABLE IF NOT EXISTS users (id int PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+        username VARCHAR(24) NOT NULL, 
+        password VARCHAR(32) NOT NULL)`;
     db.query(sql3, (err, result) => {
         if (err) throw err;
     });
 
-    let sql4 = "INSERT INTO users VALUES (NULL, 'admin', 'admin')";
+    // Add admin account to database if it doesn't already exist
+    let sql4 = `INSERT INTO users (id, username, password)
+    SELECT * FROM (SELECT NULL, 'admin', 'password') AS tmp
+    WHERE NOT EXISTS (
+        SELECT username FROM users WHERE username = 'admin'
+    ) LIMIT 1`;
+
     db.query(sql4, (err, result) => {
         if (err) throw err;
     });
