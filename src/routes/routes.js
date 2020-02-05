3f306361f72edefdd8b880117aceddcdd696f9e5
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-02-05 13:02:13 +0000

    Validation finished

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,57 @@
 // Import user routes from 'user.js'
-const userRouter = require('./user');
+const userRouter = require("./user");
 
-const appRouter = (app, fs) => {
-    app.get('/', (req, res) => {
-        res.render('home', { title: 'Home' });
+const appRouter = (app, fs, db) => {
+    app.get("/", (req, res) => {
+        res.render("home", { title: "Home" });
     });
 
-    app.get('/login', (req, res) => {
-        res.render('login', { title: 'Log In' });
+    app.get("/login", (req, res) => {
+        res.render("login", { title: "Log In" });
     });
 
-    app.post('/login', (req, res) => {});
+    app.post("/login", (req, res) => {
+        let username = req.body.username;
+        let password = req.body.password;
 
-    app.get('/register', (req, res) => {
-        res.render('register', { title: 'Register' });
+        let sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        db.query(sql, [username, password], (err, result) => {
+            if (err) throw err;
+
+            if (result.length == 0) {
+                res.render("login", {
+                    title: "Log In",
+                    response: "Invalid username / password combination."
+                });
+            } else {
+                // Log in the user
+            }
+        });
+    });
+
+    app.get("/register", (req, res) => {
+        res.render("register", { title: "Register" });
+    });
+
+    app.post("/register", (req, res) => {
+        let username = req.body.username;
+        let password = req.body.password;
+
+        let sql = "SELECT * FROM users WHERE username = ?";
+
+        db.query(sql, [username, password], (err, result) => {
+            if (err) throw err;
+
+            if (result.length > 0) {
+                res.render("register", {
+                    title: "Register",
+                    response: "Username is taken."
+                });
+            } else {
+                // Add user to database
+            }
+        });
     });
 
     // Include user routes in parent router
