3dc27a4d2e3625496f23c0115b3e6e197229167f
Jordan <19174130+jordan-git@users.noreply.github.com>
2020-02-08 13:10:09 +0000

    Edit dob on profile

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-const pw = require('password-hash');
+const pw = require("password-hash");
 
 // Helper functions
 // Return true if logged in else false
@@ -12,24 +12,24 @@ const isLoggedIn = req => {
 };
 
 const appRouter = (app, fs, db) => {
-    app.get('/', (req, res) => {
-        res.render('home', {
-            title: 'Home',
+    app.get("/", (req, res) => {
+        res.render("home", {
+            title: "Home",
             logged_in: req.session.logged_in,
             username: req.session.username
         });
     });
 
-    app.get('/login', (req, res) => {
+    app.get("/login", (req, res) => {
         // If logged in, do nothing
         if (isLoggedIn(req)) {
             return;
         }
 
-        res.render('login', { title: 'Log In' });
+        res.render("login", { title: "Log In" });
     });
 
-    app.post('/login', (req, res) => {
+    app.post("/login", (req, res) => {
         // Do nothing if already logged in
         if (isLoggedIn(req)) return;
 
@@ -38,53 +38,53 @@ const appRouter = (app, fs, db) => {
         let password = req.body.password;
 
         // Checking if the username/password combination exists
-        let sql = 'SELECT * FROM users WHERE username = ?';
+        let sql = "SELECT * FROM users WHERE username = ?";
         db.query(sql, username, (err, result) => {
             if (err) throw err;
 
             // If the user doesn't exist
             if (result.length == 0) {
-                res.render('login', {
-                    title: 'Log In',
-                    response: 'Invalid username/password combination'
+                res.render("login", {
+                    title: "Log In",
+                    response: "Invalid username/password combination"
                 });
             } else {
                 // If password matches hash
                 if (pw.verify(password, result[0].password)) {
                     req.session.logged_in = true;
                     req.session.username = result[0].username;
-                    res.redirect('/');
+                    res.redirect("/");
                 } else {
-                    res.render('login', {
-                        title: 'Log In',
-                        response: 'Invalid username/password combination'
+                    res.render("login", {
+                        title: "Log In",
+                        response: "Invalid username/password combination"
                     });
                 }
             }
         });
     });
 
-    app.get('/log-out', (req, res) => {
+    app.get("/log-out", (req, res) => {
         // If logged in, log out
         if (isLoggedIn(req)) {
             req.session.destroy();
-            res.redirect('/');
+            res.redirect("/");
         }
     });
 
-    app.get('/register', (req, res) => {
+    app.get("/register", (req, res) => {
         // If currently logged in, return to home
         if (isLoggedIn(req)) {
-            res.redirect('/');
+            res.redirect("/");
             return;
         }
-        res.render('register', { title: 'Register' });
+        res.render("register", { title: "Register" });
     });
 
-    app.post('/register', (req, res) => {
+    app.post("/register", (req, res) => {
         // If currently logged in, return to home
         if (isLoggedIn(req)) {
-            res.redirect('/');
+            res.redirect("/");
             return;
         }
 
@@ -95,29 +95,29 @@ const appRouter = (app, fs, db) => {
         let dob = `${req.body.year}-${req.body.month}-${req.body.day}`;
 
         // Checking if the username exists
-        let sql = 'SELECT * FROM users WHERE username = ?';
+        let sql = "SELECT * FROM users WHERE username = ?";
         db.query(sql, username, (err, result) => {
             if (err) throw err;
 
             // If the username exists
             if (result.length > 0) {
-                res.render('register', {
-                    title: 'Register',
-                    response: 'Username is already in use'
+                res.render("register", {
+                    title: "Register",
+                    response: "Username is already in use"
                 });
             } else {
                 // Turn password into hash and store in database
                 password = pw.generate(password);
-                sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+                sql = "INSERT INTO users (username, password) VALUES (?, ?)";
                 db.query(sql, [username, password], (err, result) => {
                     if (err) throw err;
                 });
 
-                sql = 'SELECT * FROM users WHERE username = ?';
+                sql = "SELECT * FROM users WHERE username = ?";
                 db.query(sql, username, (err, result) => {
                     if (err) throw err;
                     sql =
-                        'INSERT INTO profiles (user_id, email, dob) VALUES (?, ?, ?)';
+                        "INSERT INTO profiles (user_id, email, dob) VALUES (?, ?, ?)";
                     db.query(
                         sql,
                         [result[0].user_id, email, dob],
@@ -130,37 +130,41 @@ const appRouter = (app, fs, db) => {
                 req.session.logged_in = true;
                 req.session.username = username;
                 // TODO: Registration success message
-                res.redirect('/');
+                res.redirect("/");
             }
         });
     });
 
-    app.get('/edit-profile', (req, res) => {
+    app.get("/edit-profile", (req, res) => {
         // If not logged in, request login
         if (!isLoggedIn(req)) {
-            res.redirect('/login');
+            res.redirect("/login");
             return;
         }
 
-        let sql = 'SELECT * FROM users WHERE username = ?';
+        let sql = "SELECT * FROM users WHERE username = ?";
 
         db.query(sql, req.session.username, (err, result) => {
             if (err) throw err;
             let user_id = result[0].user_id;
             let username = result[0].username;
 
-            sql = 'SELECT * FROM profiles WHERE user_id = ?';
+            sql = "SELECT * FROM profiles WHERE user_id = ?";
             db.query(sql, user_id, (err, result) => {
                 let stage_name = result[0].stage_name;
                 let email = result[0].email;
                 // TODO: DOB !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                let dob = results[0].dob;
+                let dob = result[0].dob;
+                let year = dob.getFullYear();
+                let month = dob.getMonth();
+                let day = dob.getDate();
+                console.log(year, month, day);
                 let location = result[0].location;
                 let interests = result[0].interests;
                 let favourite_genres = result[0].favourite_genres;
 
-                res.render('edit-profile', {
-                    title: 'Edit Profile',
+                res.render("edit-profile", {
+                    title: "Edit Profile",
                     logged_in: req.session.logged_in,
                     username: username,
                     email: email,
@@ -176,9 +180,9 @@ const appRouter = (app, fs, db) => {
         });
     });
 
-    app.post('/edit-profile', (req, res) => {
+    app.post("/edit-profile", (req, res) => {
         if (!isLoggedIn(req)) {
-            res.redirect('/');
+            res.redirect("/");
             return;
         }
 
@@ -189,13 +193,13 @@ const appRouter = (app, fs, db) => {
         let email = req.body.email;
         let dob = `${req.body.year}-${req.body.month}-${req.body.day}`;
 
-        let sql = 'SELECT * FROM users WHERE username = ?';
+        let sql = "SELECT * FROM users WHERE username = ?";
         db.query(sql, req.session.username, (err, result) => {
             if (err) throw err;
             let user_id = result[0].user_id;
 
             sql =
-                'UPDATE profiles SET stage_name = ?, email = ?, dob = ?, location = ?, interests = ?, favourite_genres = ? WHERE user_id = ?';
+                "UPDATE profiles SET stage_name = ?, email = ?, dob = ?, location = ?, interests = ?, favourite_genres = ? WHERE user_id = ?";
             db.query(
                 sql,
                 [
@@ -212,7 +216,7 @@ const appRouter = (app, fs, db) => {
                 }
             );
         });
-        res.redirect('/edit-profile');
+        res.redirect("/edit-profile");
     });
 };
 
