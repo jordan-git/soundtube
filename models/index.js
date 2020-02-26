c9559a2c3fbf520001f69aac8f03fbdba0956ecf
Jordan <jordanquinlan734@gmail.com>
2020-02-26 12:18:39 +0000

    Create default user if doesn't exist

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
-const mysql = require("mysql");
-const mysql2 = require("mysql2");
-const fs = require("fs");
-const path = require("path");
-const { Sequelize } = require("sequelize");
+const mysql = require('mysql');
+const mysql2 = require('mysql2');
+const fs = require('fs');
+const path = require('path');
+const { Sequelize } = require('sequelize');
 const database = {};
 
 // Create the database connection
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password"
+    host: 'localhost',
+    user: 'root',
+    password: 'password'
 });
 
 // // Drop database every restart (for testing only)
@@ -18,30 +18,30 @@ const conn = mysql.createConnection({
 // });
 
 // Create an empty database if it doesn't exist
-conn.query("CREATE DATABASE IF NOT EXISTS soundtube;", (err, result) => {
+conn.query('CREATE DATABASE IF NOT EXISTS soundtube;', (err, result) => {
     if (err) throw err;
 });
 
 /* Connect to database with our ORM (Object-relational mapping)
 Using a ORM allows us to connect to and control our database using Javascript */
-const sequelize = new Sequelize("soundtube", "root", "password", {
-    dialect: "mysql",
+const sequelize = new Sequelize('soundtube', 'root', 'password', {
+    dialect: 'mysql',
     logging: false
 });
 
-/*  Reads every single model file in this directory excluding this one and creates an object
+/*  Reads every single file in this directory excluding this one and creates an object
     of the table described in the file, then adds each object to the dictionary 'database' */
 const basename = path.basename(__filename);
 fs.readdirSync(__dirname)
     .filter(file => {
         return (
-            file.indexOf(".") !== 0 &&
+            file.indexOf('.') !== 0 &&
             file !== basename &&
-            file.slice(-3) === ".js"
+            file.slice(-3) === '.js'
         );
     })
     .forEach(file => {
-        let model = sequelize["import"](path.join(__dirname, file));
+        let model = sequelize['import'](path.join(__dirname, file));
         database[model.name] = model;
     });
 
@@ -53,7 +53,35 @@ Object.keys(database).forEach(modelName => {
     }
 });
 
-// Attach our connection to the ORM to our database dictionary with the object for each table
+// Attach our connection to the ORM to our database object that contains the object representing each table
 database.sequelize = sequelize;
+
+// Create a default user if it doesn't exist
+async function createDefaultUser() {
+    const userExists = await database.User.findOne({
+        where: {
+            username: 'Admin'
+        }
+    });
+
+    if (userExists != null) return;
+
+    const user = await database.User.create({
+        username: 'Admin',
+        password: 'password',
+        created_at: '2020-01-01',
+        email: 'example@soundtube.ie',
+        date_of_birth: '2000-01-01'
+    });
+    await database.Profile.create({
+        user_id: user.dataValues.id,
+        stage_name: 'Stage Name',
+        location: 'Dublin, Ireland',
+        interests: 'Music',
+        favourite_genres: 'Rap, Jazz'
+    });
+}
+
+createDefaultUser();
 
 module.exports = database;
