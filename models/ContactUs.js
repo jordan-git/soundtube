6f45a6629b34343a7111ba64428fc1a375a53323
rossb11 <rossbucka2011@gmail.com>
2020-04-15 19:00:26 +0100

    changes

---
+++ models/ContactUs.js
@@ -0,0 +1,19 @@
+module.exports = sequelize => {
+    const {DataTypes} = require ("sequelize");
+
+const ContactUs = sequelize.define(
+    'ContactUs',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+    },
+        name: { type: DataTypes.STRING(32) },
+        email: { type: DataTypes.STRING(48)},
+        message: { type: DataTypes.STRING(128)},
+},
+     { timestamps: false, tableName: 'ContactUs', underscored: true }
+    )
+};
+  
